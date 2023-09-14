2e7d8bc5c8f6e1b91b82b10cc21533931803fe82
lipar2 <sukurmarinko4@gmail.com>
2023-09-14 17:35:31 +0200

    Initial commit

---
+++ src/paket/Prvaklasa.java
@@ -0,0 +1,16 @@
+package paket;
+
+public class Prvaklasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//prvi komentar 
+		
+		/*vise
+		 * linijski
+		 * komentar
+		 */
+		
+	}
+
+}
