332d6d62d93b8a35bfcb1fa4bbb14f906e18c6ca
lipar2 <sukurmarinko4@gmail.com>
2023-09-14 18:34:21 +0200

    zadatak1

--- src/paket/Prvaklasa.java
+++ src/paket/Prvaklasa.java
@@ -1,5 +1,7 @@
 package paket;
 
+import java.util.Scanner;
+
 public class Prvaklasa {
 
 	public static void main(String[] args) {
@@ -10,7 +12,17 @@ public class Prvaklasa {
 		 * linijski
 		 * komentar
 		 */
-		
-	}
 
-}
+		        Scanner scanner = new Scanner(System.in);
+		        int n = Integer.parseInt(scanner.nextLine());
+		        int[] a = new int[n];
+		        for (int i = 0; i < a.length; i++) {
+		            a[i] = Integer.parseInt(scanner.nextLine());
+		        }
+		        for (int i = 0; i < a.length; i++) {
+		            if (a[i] % 2 == 0) {
+		                System.out.println(a[i]);
+		            }
+		        }
+		    }
+		}
